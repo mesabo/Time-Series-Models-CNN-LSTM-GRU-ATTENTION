3ca978d8a5b22eacfc070e6b1a76ae2afebac0f5
MESSOU FRANCK <46136933+mesabo@users.noreply.github.com>
2024-03-24 17:18:19 +0900

    Update run-sample-gpux1.sh

--- run-sample-gpux1.sh
+++ run-sample-gpux1.sh
@@ -6,6 +6,6 @@ hostname
 echo USED GPUs=$CUDA_VISIBLE_DEVICES
 source  activate tf-gpu
 pwd
-cd /home/23r9802_chen/messou/multivariate_time_series/
+cd /home/users/messou/multivariate_time_series/
 
 python ./src/sample.py
